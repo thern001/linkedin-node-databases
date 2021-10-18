1330e03a67f03e0fed5efb6a38248e1204bee3a8
thern001 <thern001@gmail.com>
2021-10-18 11:12:30 -0400

    connected to mongo db and then disconnected

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -17,18 +17,42 @@ class MongoBackend {
   async connect() {
     const mongoClient = new MongoClient(this.mongoUrl, {
       useUnifiedTopology: true,
-      userNewUrlParser: true,
+      useNewUrlParser: true,
     });
-    this.client = await mongoClient.connect();
+    try {
+      this.client = await mongoClient.connect();
+      this.collection = this.client.db("maxcoin").collection("values");
+      return this.client;
+    } catch (error) {
+      console.log("ERROR aquiring DB Connection!");
+      console.log(error);
+      throw error;
+    }
   }
 
-  async disconnect() {}
+  async disconnect() {
+    if (this.client) {
+      return this.client.close();
+    }
+    return false;
+  }
 
   async insert() {}
 
   async getMax() {}
 
-  async max() {}
+  async max() {
+    console.info("Connection to MongoDb");
+    console.time("mongodb-connect");
+    const client = await this.connect();
+    console.info("Successfully connected to MongoDb");
+    console.timeEnd("mongodb-connect");
+
+    console.info("Disconnecting from MongoDb");
+    console.time("mongodb-disconnect");
+    await this.disconnect();
+    console.timeEnd("mongodb-disconnect");
+  }
 }
 
 module.exports = MongoBackend;
