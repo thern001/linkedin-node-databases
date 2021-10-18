6eb5008665cbcbd148b34ea5df5e139510d0d725
thern001 <thern001@gmail.com>
2021-10-18 10:08:04 -0400

    Connected to Mongo Db
    This is the point where I setup a mongo db connection.  https://www.linkedin.com/learning/databases-for-node-js-developers-2/connect-to-mongodb?autoAdvance=true&autoSkip=true&autoplay=true&contextUrn=urn%3Ali%3AlyndaLearningPath%3A5ade1c55498e39379949fb9c&resume=false&u=15599356

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,15 +1,26 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+
+const { MongoClient } = require("mongodb");
+
+const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      userNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+  }
 
   async disconnect() {}
 
